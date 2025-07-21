6d5b2ec02bd92ee73babd25f1048304c29d5656c
Abdeali Jaroli <abdealiburu03@gmail.com>
2025-07-21 11:07:38 +0530

    build: include storage dir

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Create storage directory for SQLite
+mkdir -p storage
+
+# Continue with regular build
 bundle install
 bundle exec rails db:migrate
 bundle exec rails assets:precompile
